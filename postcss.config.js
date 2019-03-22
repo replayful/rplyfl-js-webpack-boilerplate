404afa3b15fabd7fa03d96c06616ec21993ed5f3
Guillermo <215023+gplatas@users.noreply.github.com>
2019-03-22 10:57:24 -0300

    removed tailwindcss

--- postcss.config.js
+++ postcss.config.js
@@ -3,38 +3,33 @@
 // eslint-disable-next-line no-undef
 
 
-// module.exports = {
-//   plugins: {
-//     'postcss-import': {},
-//     'postcss-nested': {},
-//     'postcss-inherit': {},
-//     'postcss-typescale': {},
-//     'postcss-responsive-type': {},
-//     'postcss-preset-env': {
-//       stage: 1,
-//       features: {
-//         'nesting-rules': true,
-//         'color-mod-function': true,
-//         'custom-properties': {
-//           preserve: false,
-//           importFrom: 'src/css/vars.css',
-//         },
-//         'custom-selectors': {
-//           preserve: false,
-//         },
-//         'custom-media-queries': true,
-//       },
-//     },
-//     'postcss-calc': {},
-//     cssnano: {
-//       calc: { precision: 2 },
-//     },
-//   },
-// };
-
 module.exports = {
-  plugins: [
-    require('postcss-import')({}),
-    require('tailwindcss')('./tailwind.js'),
-  ],
+  plugins: {
+    'postcss-import': {},
+    'postcss-nested': {},
+    'postcss-inherit': {},
+    'postcss-typescale': {},
+    'postcss-responsive-type': {},
+    'postcss-preset-env': {
+      stage: 1,
+      features: {
+        'nesting-rules': true,
+        'color-mod-function': true,
+        'custom-properties': {
+          preserve: false,
+          importFrom: 'src/css/vars.css',
+        },
+        'custom-selectors': {
+          preserve: false,
+        },
+        'custom-media-queries': true,
+      },
+    },
+    'postcss-calc': {},
+    cssnano: {
+      calc: { precision: 2 },
+    },
+  },
 };
+
+
